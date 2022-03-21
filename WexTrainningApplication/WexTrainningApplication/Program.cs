d3aa21289b95702bcd7a44ca82f40844d45d0b29
Lena Schurko <LenaSchurko@coherentsolutions.com>
2022-03-21 07:58:58 +0300

    My 1st HomeWork

---
+++ WexTrainningApplication/WexTrainningApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+int maxIntValue = int.MaxValue;
+int minIntValue = int.MinValue;
+Console.WriteLine("Int Max Value = {0}, Int Min Value = {1}", maxIntValue, minIntValue);
+
+byte maxByteValue = byte.MaxValue;
+byte minByteValue = byte.MinValue;
+Console.WriteLine("Byte Max Value = {0}, Byte Min Value = {1}", maxByteValue,minByteValue);
+
+short maxShortValue = short.MaxValue;
+short minShortValue = short.MinValue;
+Console.WriteLine("Short Max Value = {0}, Short Min Value = {1}", maxShortValue,minShortValue);
+
+long maxLongValue = long.MaxValue;
+long minLongValue = long.MinValue;
+Console.WriteLine("Long Max Value = {0}, Long Min Value = {1}", maxLongValue,minLongValue);
+
+float maxFloatValue = float.MaxValue;
+float minFloatValue = float.MinValue;
+Console.WriteLine("Float Max Value = {0}, Float Min Value = {1}", maxFloatValue,minFloatValue);
+
+double maxDoubleValue = double.MaxValue;
+double minDoubleValue = double.MinValue;
+System.Console.WriteLine("Double Max Value = {0}, Double Min Value = {1}", maxDoubleValue, minDoubleValue);
+
+uint maxUIntValue = uint.MaxValue;
+uint minUIntValue = uint.MinValue;
+Console.WriteLine("Uint Max Value = {0}, Uint Min Value = {1}", maxUIntValue,minUIntValue);
+
+ulong maxULongValue = ulong.MaxValue;
+ulong minULongValue = ulong.MinValue;
+Console.WriteLine($"Ulong Max Value = {maxULongValue}, Ulong Min Value = {minULongValue}");
+
