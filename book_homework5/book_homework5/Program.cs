180b43f14c16b6276d303e553291ae1652e353f6
Lena Schurko <LenaSchurko@coherentsolutions.com>
2022-03-21 08:30:52 +0300

    My 5th Homework

---
+++ book_homework5/book_homework5/Program.cs
@@ -0,0 +1,22 @@
+﻿using book_homework5;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Book book1 = new() { Title = "Neznajka", Pages = 167, Indentificator = "123ADF125av4" };
+        Book book2 = new() { Title = "Vij", Pages = 58, Indentificator = "dvdffv12ds" };
+        Book book3 = new() { Title = "Tom Sojer", Pages = 132, Indentificator = "987654dvd" };
+        Book book4 = new() { Title = "Dinka", Pages = 334, Indentificator = "85jfuie45" };
+        Book book5 = new() { Title = "ABC", Pages = 88, Indentificator = "09dneuie34" };
+
+        Book[] books = { book1, book2, book3, book4, book5 };
+
+        foreach (Book item in books)
+        {
+            item.DisplayBook();
+
+        }
+      
+    }
+}
