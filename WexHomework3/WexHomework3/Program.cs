5699669ebeaf2dee78eb16966cfff63fcb0249b5
Lena Schurko <LenaSchurko@coherentsolutions.com>
2022-03-21 08:17:14 +0300

    My 3rd Homework

---
+++ WexHomework3/WexHomework3/Program.cs
@@ -0,0 +1,37 @@
+﻿static void IntializeArray(int[] array,int cofficient)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = i;
+
+        int k = 1;
+        while (k<cofficient)
+        {
+            array[i] = array[i]*i;
+            k++;
+        }
+    }
+
+}
+
+static float avgCalculation(int[] arrayAvg)
+{
+    float avgs = 0;
+    for (int e = 0; e < arrayAvg.Length; e++)
+    {
+        avgs  += arrayAvg[e];
+    }
+    avgs = avgs / arrayAvg.Length;
+
+    return avgs;
+}
+ 
+for(int z = 1; z<=3;z++)
+{ 
+    int[] arrayF=new int[10];
+    IntializeArray(arrayF, z);
+    Console.WriteLine("Average array{0}  {1}", z, avgCalculation(arrayF));
+}
+
+
+
