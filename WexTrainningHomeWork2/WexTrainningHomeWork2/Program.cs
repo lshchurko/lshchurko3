21dec0cb6f906179fc7c3ee9804881135d641d4e
Lena Schurko <LenaSchurko@coherentsolutions.com>
2022-03-21 08:11:04 +0300

    My 2nd Home work

---
+++ WexTrainningHomeWork2/WexTrainningHomeWork2/Program.cs
@@ -0,0 +1,12 @@
+﻿//HomeWork Task2
+
+int[] array= new int[10];
+
+for (int index = 0; index < array.Length; index++)
+{
+    array[index] = index*index;
+    Console.WriteLine(array[index]);
+}
+
+
+
